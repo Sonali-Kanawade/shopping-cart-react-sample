bfc2b1561ef6bf2c9da3f1a2697e64057fa2e553
Sonali Kanawade <sonalikanawade111@gmail.com>
2025-07-05 08:55:35 +0530

    package changes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 
-test('renders learn react link', () => {
+const routes = [
+  {
+    path: '/',
+    element: <div>Home Route</div>,
+  },
+];
+
+
+test('renders learn react link', async() => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // Replace this with actual text expected on your default route
+    const element = await screen.findByText(/home/i); // e.g., "Home Page"
+    expect(element).toBeInTheDocument();
+});
+
+
+test('renders route via RouterProvider', async () => {
+  const router = createMemoryRouter(routes, { initialEntries: ['/'] });
+
+  render(<RouterProvider router={router} />);
+
+  expect(await screen.findByText('Home Route')).toBeInTheDocument();
 });
